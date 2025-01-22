e71f1e8426873bdc5ac396077e960b10faf9986e
pradnya029 <pradnya.jadhav23@pcu.edu.in>
2025-01-22 13:30:26 +0530

    Added greetUser function to app.js

--- hello-nodejs/app.js
+++ hello-nodejs/app.js
@@ -1,2 +1,13 @@
-console.log("Hello World!\n");
+// Existing code
+console.log("Hello, Node.js!");
+
+// New function to greet a user
+function greetUser(name) {
+    return `Hello, ${name}! Welcome to Node.js.`;
+}
+
+// Example usage of the new function
+const userName = "Pradnya";
+console.log(greetUser(userName));
+
 
